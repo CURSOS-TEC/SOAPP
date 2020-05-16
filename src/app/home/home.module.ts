dfe18e1c1eee440c79723cf6575e4e7253d5a55f
Juan Esteban Navarro Camacho <jnavcamacho@gmail.com>
2020-05-15 20:32:00 -0600

    [Add] Stable image

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { File } from '@ionic-native/file/ngx';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
 
 
 @NgModule({
@@ -14,6 +16,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [File,ImagePicker]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { File } from '@ionic-native/file/ngx';
+import { Platform } from '@ionic/angular';
+import { ImagePicker, OutputType } from '@ionic-native/image-picker/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  imageData: string;
+  imageName: string;
+  disableButtonSend: boolean;
 
-  constructor() {}
+  constructor(private file: File, public platform: Platform, private imagePicker: ImagePicker) {
+    this.disableButtonSend = true;
+  }
+  
+  ngOnInit() {
+    
+    this.imageData = './../../assets/icon/icon.png';
+  }
+
+  readImage(event: CustomEvent) {
+    console.log(event);
+    this.imageName = event.detail.value;
+  }
+
+
+  readFile() {
+    this.platform.ready().then(
+      ready => {
+        if (this.platform.is('cordova')) {
+          this.imagePicker.hasReadPermission().then(
+            perm => {
+              this.imagePicker.getPictures({
+                maximumImagesCount: 10,
+                width: 800,
+                outputType: OutputType.DATA_URL
+              }).then(images => {
+                console.log(images);
+                this.imageData = `data:image/png;base64, ${images[0]}`;
+                this.disableButtonSend = false;
+              });
+            }
+          );
+        }
+      });
+  }
+
+  sendFile() {
+
+
+  }
+
+
 
 }
